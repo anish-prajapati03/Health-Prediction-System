b22961d09a5aeeef6c0c6e0a9c25369b7be703f5
Anish Prajapati <anish.prajapati7752@gmail.com>
2026-03-10 22:17:54 +0530

    deployment setup

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sat92++qcmiy%$per0pbg+-3$8zpe2zn2gyv!i)iy91zj0i&r*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = 'staticfiles'
